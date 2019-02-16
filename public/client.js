8fbf139883c523adeda8bd55567ddafcc0247a1e
Glitch (hello-sqlite) <none>
2019-02-16 06:49:03 +0000

    🍻📠 Checkpoint ./public/client.js:147888/618 ./views/index.html:147888/94

--- public/client.js
+++ public/client.js
@@ -1,60 +1,59 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
 
-let dreams = [];
+let datapoints = [];
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
+const datapointsList = document.getElementById('datapoints');
+const datapointsForm = document.forms[0];
+const datapointInput = datapointsForm.elements['datapoint'];
 
-// a helper function to call when our request for dreams is done
-const getDreamsListener = function() {
+// a helper function to call when our request for datapoints is done
+const getDatapointsListener = function() {
   // parse our response to convert to JSON
-  dreams = JSON.parse(this.responseText);
+  datapoints = JSON.parse(this.responseText);
 
-  // iterate through every dream and add it to our page
-  dreams.forEach( function(row) {
-    appendNewDream(row.datapoint);
+  // iterate through every datapoint and add it to our page
+  datapoints.forEach( function(row) {
+    appendNewDatapoint(row.datapoint);
   });
 }
 
-const addDreamListener = function() {
+const addDatapointListener = function() {
   // parse our response to convert to JSON
   console.log(this.responseText);
 }
 
-// request the dreams from our app's sqlite database
-const dreamRequest = new XMLHttpRequest();
-dreamRequest.onload = getDreamsListener;
-dreamRequest.open('get', '/getDatapoints');
-dreamRequest.send();
+// request the datapoints from our app's sqlite database
+const datapointRequest = new XMLHttpRequest();
+datapointRequest.onload = getDatapointsListener;
+datapointRequest.open('get', '/getDatapoints');
+datapointRequest.send();
   
-const dreamAddRequest = new XMLHttpRequest();
-dreamAddRequest.onload = addDreamListener;
+const datapointAddRequest = new XMLHttpRequest();
+datapointAddRequest.onload = addDatapointListener;
 // assumed it went through...
 
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
+// a helper function that creates a list item for a given datapoint
+const appendNewDatapoint = function(datapoint) {
   const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
+  newListItem.innerHTML = datapoint;
+  datapointsList.appendChild(newListItem);
 }
 
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
+// listen for the form to be submitted and add a new datapoint when it is
+datapointsForm.onsubmit = function(event) {
   // stop our form submission from refreshing the page
   event.preventDefault();
 
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-  //dreamAddRequest.open('get', '/addDatapoint', {datapoint: dreamInput.value} );
-  dreamAddRequest.open('get', '/addDatapoint' + "?datapoint=" + dreamInput.value} );
-  "?author=" + param), true
-  dreamAddRequest.send();
+  // get datapoint value and add it to the list
+  datapoints.push(datapointInput.value);
+  appendNewDatapoint(datapointInput.value);
+  //datapointAddRequest.open('get', '/addDatapoint', {datapoint: datapointInput.value} );
+  datapointAddRequest.open('get', '/addDatapoint' + "?datapoint=" + encodeURIComponent(datapointInput.value), true );
+  datapointAddRequest.send();
 
   // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
+  datapointInput.value = '';
+  datapointInput.focus();
 };
